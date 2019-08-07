8764f5e375099995c55bdab2b0eaae270ef6c1fb
khalidwork <53859453+khalidwork@users.noreply.github.com>
2019-08-07 14:10:44 +0100

    Update Demo.java

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -3,10 +3,10 @@ package com.microsoft.demo;
 public class Demo {
     public void DoSomething(boolean flag){
         if(flag){
-            System.out.println("I am covered");
+            System.out.println("Adding code coverage in Maven and Gradle prijecs --> I am covered DP");
             return;
         }
 
-        System.out.println("I am not covered");
+        System.out.println("I am not covered DP");
     }
 }
